cc7b603ba1f13670e955d16e20c9cbe13e262a68
akristianlopez <30592645+akristianlopez@users.noreply.github.com>
2025-11-28 19:33:08 +0100

    Update the list of tokens
    Update the list of tokens

--- token.go
+++ token.go
@@ -1,433 +1,270 @@
 package token
 
-/**
- * This class implements a node of the list that returned by the scanner after it performing.
- * This node is used by the lexer during its performing
- * @author Christian Lopez ATOUBA
- */
-
-// Token représente un token lexical
-type Token struct {
-	Type   TokenType
-	Value  string
-	Line   int
-	Column int
-}
-
-// TokenType représente le type de token
 type TokenType int
 
 const (
-	// Tokens spéciaux
-	TOKEN_EOF TokenType = iota
-	TOKEN_NEWLINE
-	TOKEN_ERROR
+	// Mots-clés
+	ACTION TokenType = iota + 1000
+	START
+	STOP
+	END
+	IF
+	ELSE
+	WHILE
+	FOR
+	FOREACH
+	SWITCH
+	CASE
+	DEFAULT
+	LET
+	CONST
+	STRUCT
+	FUNCTION
+	RETURN
+	SELECT
+	FROM
+	WHERE
+	ORDER
+	BY
+	ASC
+	DESC
+	LIMIT
+	OFFSET
+	AS
+	INTO
+	OBJET
+	INNER
+	LEFT
+	RIGHT
+	JOIN
+	ON
+	GROUP
+	HAVING
+	LIKE
+	BETWEEN
+	IS
+	NULL
+	UNION
+	ALL
+	WITH
+	RECURSIVE
+	DISTINCT
+	CREATE
+	DROP
+	DELETE
+	UPDATE
+	SAVE
+	SET
+	VIEW
+	INDEX
+	SEQUENCE
+	NOT
+	DEFAULT_KW
+	INTEGER
+	BOOLEAN
+	FLOAT_TYPE
+	STRING_TYPE
+	DATE
+	TIME
+	DATETIME
+	DURATION
+	ARRAY
+	DIGITS
+	LENGTH
+	MIN
+	MAX
+	RANGE
+	INTEGER_DIGITS
+	DECIMAL_DIGITS
+	OPTIONAL
+	SELF
+	YEARS
+	MONTHS
+	DAYS
+	HOURS
+	MINUTES
+	SECONDS
+	MILLISECONDS
+	TRUE
+	FALSE
+	AND
+	OR
+	NOT_OP
+	IN
+	NEWLINE
+	EOF
 
-	// Mots-clés principaux
-	TOKEN_ACTION
-	TOKEN_START
-	TOKEN_STOP
-	TOKEN_END
+	// Identifiants et littéraux
+	IDENTIFIER
+	NUMBER
+	FLOAT
+	STRING
+	DATE_LITERAL
+	TIME_LITERAL
+	DURATION_LITERAL
 
-	// Structures de contrôle
-	TOKEN_IF
-	TOKEN_ELSE
-	TOKEN_WHILE
-	TOKEN_FOR
-	TOKEN_FOREACH
-	TOKEN_SWITCH
-	TOKEN_CASE
-	TOKEN_DEFAULT
-	TOKEN_DO
-	TOKEN_THEN
-
-	// Déclarations
-	TOKEN_LET
-	TOKEN_CONST
-	TOKEN_STRUCT
-	TOKEN_FUNCTION
-	TOKEN_RETURN
-	TOKEN_TYPE
-
-	// SQL et bases de données
-	TOKEN_SELECT
-	TOKEN_FROM
-	TOKEN_WHERE
-	TOKEN_ORDER
-	TOKEN_BY
-	TOKEN_ASC
-	TOKEN_DESC
-	TOKEN_LIMIT
-	TOKEN_OFFSET
-	TOKEN_AS
-	TOKEN_INTO
-	TOKEN_OBJET
-	TOKEN_INNER
-	TOKEN_LEFT
-	TOKEN_RIGHT
-	TOKEN_JOIN
-	TOKEN_ON
-	TOKEN_GROUP
-	TOKEN_HAVING
-
-	// Opérateurs SQL
-	TOKEN_LIKE
-	TOKEN_BETWEEN
-	TOKEN_IS
-	TOKEN_NULL
-	TOKEN_UNION
-	TOKEN_ALL
-	TOKEN_WITH
-	TOKEN_RECURSIVE
-	TOKEN_DISTINCT
-
-	// DDL/DML
-	TOKEN_CREATE
-	TOKEN_DROP
-	TOKEN_DELETE
-	TOKEN_UPDATE
-	TOKEN_SAVE
-	TOKEN_SET
-	TOKEN_VIEW
-	TOKEN_INDEX
-	TOKEN_SEQUENCE
-	TOKEN_NOT
-	TOKEN_RARROW
-	TOKEN_LARROW
-	// TOKEN_DEFAULT
-
-	// Types de base
-	TOKEN_INTEGER
-	TOKEN_BOOLEAN
-	TOKEN_FLOAT_TYPE
-	TOKEN_STRING_TYPE
-	TOKEN_DATE
-	TOKEN_TIME
-	TOKEN_DATETIME
-	TOKEN_DURATION
-	TOKEN_ARRAY
-
-	// Contraintes de types
-	TOKEN_DIGITS
-	TOKEN_LENGTH
-	TOKEN_MIN
-	TOKEN_MAX
-	TOKEN_RANGE
-	TOKEN_INTEGER_DIGITS
-	TOKEN_DECIMAL_DIGITS
-
-	// Structures récursives
-	TOKEN_OPTIONAL
-	TOKEN_SELF
-
-	// Unités de durée
-	TOKEN_YEARS
-	TOKEN_MONTHS
-	TOKEN_DAYS
-	TOKEN_HOURS
-	TOKEN_MINUTES
-	TOKEN_SECONDS
-	TOKEN_MILLISECONDS
-
-	// Valeurs littérales
-	TOKEN_TRUE
-	TOKEN_FALSE
-
-	// Opérateurs logiques
-	TOKEN_AND
-	TOKEN_OR
-	TOKEN_NOT_OP
-
-	// Autres mots-clés
-	TOKEN_IN
-
-	// Littéraux
-	TOKEN_IDENTIFIER
-	TOKEN_NUMBER
-	TOKEN_FLOAT
-	TOKEN_STRING
-	TOKEN_DATE_LITERAL
-	TOKEN_TIME_LITERAL
-	TOKEN_DURATION_LITERAL
-	TOKEN_BROWSE
-
-	// Opérateurs
-	TOKEN_PLUS
-	TOKEN_MINUS
-	TOKEN_MULTIPLY
-	TOKEN_DIVIDE
-	TOKEN_MODULO
-	TOKEN_ASSIGN
-	TOKEN_EQUAL
-	TOKEN_NOT_EQUAL
-	TOKEN_LESS
-	TOKEN_LESS_EQUAL
-	TOKEN_GREATER
-	TOKEN_GREATER_EQUAL
-
-	// Délimiteurs
-	TOKEN_LPAREN
-	TOKEN_RPAREN
-	TOKEN_LBRACE
-	TOKEN_RBRACE
-	TOKEN_LBRACKET
-	TOKEN_RBRACKET
-	TOKEN_SEMICOLON
-	TOKEN_COMMA
-	TOKEN_COLON
-	TOKEN_DOT
-	TOKEN_LESS_ANGLE
-	TOKEN_GREATER_ANGLE
+	// Opérateurs et ponctuation
+	PLUS
+	MINUS
+	MULTIPLY
+	DIVIDE
+	MODULO
+	ASSIGN
+	EQUAL
+	NOT_EQUAL
+	LESS
+	LESS_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LPAREN
+	RPAREN
+	LBRACE
+	RBRACE
+	LBRACKET
+	RBRACKET
+	SEMICOLON
+	COMMA
+	COLON
+	DOT
 )
 
-// String retourne la représentation textuelle du token
-func (t TokenType) String() string {
-	return tokenNames[t]
+type Token struct {
+	Type  TokenType
+	Value interface{}
+	Line  int
+	Pos   int
 }
 
 var tokenNames = map[TokenType]string{
-	TOKEN_EOF:     "EOF",
-	TOKEN_NEWLINE: "NEWLINE",
-	TOKEN_ERROR:   "ERROR",
+	ACTION:         "ACTION",
+	START:          "START",
+	STOP:           "STOP",
+	END:            "END",
+	IF:             "IF",
+	ELSE:           "ELSE",
+	WHILE:          "WHILE",
+	FOR:            "FOR",
+	FOREACH:        "FOREACH",
+	SWITCH:         "SWITCH",
+	CASE:           "CASE",
+	DEFAULT:        "DEFAULT",
+	LET:            "LET",
+	CONST:          "CONST",
+	STRUCT:         "STRUCT",
+	FUNCTION:       "FUNCTION",
+	RETURN:         "RETURN",
+	SELECT:         "SELECT",
+	FROM:           "FROM",
+	WHERE:          "WHERE",
+	ORDER:          "ORDER",
+	BY:             "BY",
+	ASC:            "ASC",
+	DESC:           "DESC",
+	LIMIT:          "LIMIT",
+	OFFSET:         "OFFSET",
+	AS:             "AS",
+	INTO:           "INTO",
+	OBJET:          "OBJET",
+	INNER:          "INNER",
+	LEFT:           "LEFT",
+	RIGHT:          "RIGHT",
+	JOIN:           "JOIN",
+	ON:             "ON",
+	GROUP:          "GROUP",
+	HAVING:         "HAVING",
+	LIKE:           "LIKE",
+	BETWEEN:        "BETWEEN",
+	IS:             "IS",
+	NULL:           "NULL",
+	UNION:          "UNION",
+	ALL:            "ALL",
+	WITH:           "WITH",
+	RECURSIVE:      "RECURSIVE",
+	DISTINCT:       "DISTINCT",
+	CREATE:         "CREATE",
+	DROP:           "DROP",
+	DELETE:         "DELETE",
+	UPDATE:         "UPDATE",
+	SAVE:           "SAVE",
+	SET:            "SET",
+	VIEW:           "VIEW",
+	INDEX:          "INDEX",
+	SEQUENCE:       "SEQUENCE",
+	NOT:            "NOT",
+	DEFAULT_KW:     "DEFAULT",
+	INTEGER:        "INTEGER",
+	BOOLEAN:        "BOOLEAN",
+	FLOAT_TYPE:     "FLOAT_TYPE",
+	STRING_TYPE:    "STRING_TYPE",
+	DATE:           "DATE",
+	TIME:           "TIME",
+	DATETIME:       "DATETIME",
+	DURATION:       "DURATION",
+	ARRAY:          "ARRAY",
+	DIGITS:         "DIGITS",
+	LENGTH:         "LENGTH",
+	MIN:            "MIN",
+	MAX:            "MAX",
+	RANGE:          "RANGE",
+	INTEGER_DIGITS: "INTEGER_DIGITS",
+	DECIMAL_DIGITS: "DECIMAL_DIGITS",
+	OPTIONAL:       "OPTIONAL",
+	SELF:           "SELF",
+	YEARS:          "YEARS",
+	MONTHS:         "MONTHS",
+	DAYS:           "DAYS",
+	HOURS:          "HOURS",
+	MINUTES:        "MINUTES",
+	SECONDS:        "SECONDS",
+	MILLISECONDS:   "MILLISECONDS",
+	TRUE:           "TRUE",
+	FALSE:          "FALSE",
+	AND:            "AND",
+	OR:             "OR",
+	NOT_OP:         "NOT_OP",
+	IN:             "IN",
+	NEWLINE:        "NEWLINE",
+	EOF:            "EOF",
 
-	TOKEN_ACTION: "ACTION",
-	TOKEN_START:  "START",
-	TOKEN_STOP:   "STOP",
-	TOKEN_END:    "END",
+	IDENTIFIER:       "IDENTIFIER",
+	NUMBER:           "NUMBER",
+	FLOAT:            "FLOAT",
+	STRING:           "STRING",
+	DATE_LITERAL:     "DATE_LITERAL",
+	TIME_LITERAL:     "TIME_LITERAL",
+	DURATION_LITERAL: "DURATION_LITERAL",
 
-	TOKEN_IF:      "IF",
-	TOKEN_ELSE:    "ELSE",
-	TOKEN_WHILE:   "WHILE",
-	TOKEN_FOR:     "FOR",
-	TOKEN_FOREACH: "FOREACH",
-	TOKEN_SWITCH:  "SWITCH",
-	TOKEN_CASE:    "CASE",
-	TOKEN_DEFAULT: "DEFAULT",
-	TOKEN_DO:      "DO",
-	TOKEN_THEN:    "THEN",
-	TOKEN_LARROW:  "<-",
-	TOKEN_RARROW:  "->",
-	TOKEN_BROWSE:  "BROWSE",
-
-	TOKEN_LET:      "LET",
-	TOKEN_CONST:    "CONST",
-	TOKEN_STRUCT:   "STRUCT",
-	TOKEN_FUNCTION: "FUNCTION",
-	TOKEN_RETURN:   "RETURN",
-	TOKEN_TYPE:     "type",
-
-	TOKEN_SELECT: "SELECT",
-	TOKEN_FROM:   "FROM",
-	TOKEN_WHERE:  "WHERE",
-	TOKEN_ORDER:  "ORDER",
-	TOKEN_BY:     "BY",
-	TOKEN_ASC:    "ASC",
-	TOKEN_DESC:   "DESC",
-	TOKEN_LIMIT:  "LIMIT",
-	TOKEN_OFFSET: "OFFSET",
-	TOKEN_AS:     "AS",
-	TOKEN_INTO:   "INTO",
-	TOKEN_OBJET:  "OBJET",
-	TOKEN_INNER:  "INNER",
-	TOKEN_LEFT:   "LEFT",
-	TOKEN_RIGHT:  "RIGHT",
-	TOKEN_JOIN:   "JOIN",
-	TOKEN_ON:     "ON",
-	TOKEN_GROUP:  "GROUP",
-	TOKEN_HAVING: "HAVING",
-
-	TOKEN_LIKE:      "LIKE",
-	TOKEN_BETWEEN:   "BETWEEN",
-	TOKEN_IS:        "IS",
-	TOKEN_NULL:      "NULL",
-	TOKEN_UNION:     "UNION",
-	TOKEN_ALL:       "ALL",
-	TOKEN_WITH:      "WITH",
-	TOKEN_RECURSIVE: "RECURSIVE",
-	TOKEN_DISTINCT:  "DISTINCT",
-
-	TOKEN_CREATE:   "CREATE",
-	TOKEN_DROP:     "DROP",
-	TOKEN_DELETE:   "DELETE",
-	TOKEN_UPDATE:   "UPDATE",
-	TOKEN_SAVE:     "SAVE",
-	TOKEN_SET:      "SET",
-	TOKEN_VIEW:     "VIEW",
-	TOKEN_INDEX:    "INDEX",
-	TOKEN_SEQUENCE: "SEQUENCE",
-
-	TOKEN_INTEGER:     "INTEGER",
-	TOKEN_BOOLEAN:     "BOOLEAN",
-	TOKEN_FLOAT_TYPE:  "FLOAT_TYPE",
-	TOKEN_STRING_TYPE: "STRING_TYPE",
-	TOKEN_DATE:        "DATE",
-	TOKEN_TIME:        "TIME",
-	TOKEN_DATETIME:    "DATETIME",
-	TOKEN_DURATION:    "DURATION",
-	TOKEN_ARRAY:       "ARRAY",
-
-	TOKEN_DIGITS:         "DIGITS",
-	TOKEN_LENGTH:         "LENGTH",
-	TOKEN_MIN:            "MIN",
-	TOKEN_MAX:            "MAX",
-	TOKEN_RANGE:          "RANGE",
-	TOKEN_INTEGER_DIGITS: "INTEGER_DIGITS",
-	TOKEN_DECIMAL_DIGITS: "DECIMAL_DIGITS",
-
-	TOKEN_OPTIONAL: "OPTIONAL",
-	TOKEN_SELF:     "SELF",
-
-	TOKEN_YEARS:        "YEARS",
-	TOKEN_MONTHS:       "MONTHS",
-	TOKEN_DAYS:         "DAYS",
-	TOKEN_HOURS:        "HOURS",
-	TOKEN_MINUTES:      "MINUTES",
-	TOKEN_SECONDS:      "SECONDS",
-	TOKEN_MILLISECONDS: "MILLISECONDS",
-
-	TOKEN_TRUE:  "TRUE",
-	TOKEN_FALSE: "FALSE",
-
-	TOKEN_AND:    "AND",
-	TOKEN_OR:     "OR",
-	TOKEN_NOT_OP: "NOT_OP",
-
-	TOKEN_IN: "IN",
-
-	TOKEN_IDENTIFIER:       "IDENTIFIER",
-	TOKEN_NUMBER:           "NUMBER",
-	TOKEN_FLOAT:            "FLOAT",
-	TOKEN_STRING:           "STRING",
-	TOKEN_DATE_LITERAL:     "DATE_LITERAL",
-	TOKEN_TIME_LITERAL:     "TIME_LITERAL",
-	TOKEN_DURATION_LITERAL: "DURATION_LITERAL",
-
-	TOKEN_PLUS:          "PLUS",
-	TOKEN_MINUS:         "MINUS",
-	TOKEN_MULTIPLY:      "MULTIPLY",
-	TOKEN_DIVIDE:        "DIVIDE",
-	TOKEN_MODULO:        "MODULO",
-	TOKEN_ASSIGN:        "ASSIGN",
-	TOKEN_EQUAL:         "EQUAL",
-	TOKEN_NOT_EQUAL:     "NOT_EQUAL",
-	TOKEN_LESS:          "LESS",
-	TOKEN_LESS_EQUAL:    "LESS_EQUAL",
-	TOKEN_GREATER:       "GREATER",
-	TOKEN_GREATER_EQUAL: "GREATER_EQUAL",
-
-	TOKEN_LPAREN:        "LPAREN",
-	TOKEN_RPAREN:        "RPAREN",
-	TOKEN_LBRACE:        "LBRACE",
-	TOKEN_RBRACE:        "RBRACE",
-	TOKEN_LBRACKET:      "LBRACKET",
-	TOKEN_RBRACKET:      "RBRACKET",
-	TOKEN_SEMICOLON:     "SEMICOLON",
-	TOKEN_COMMA:         "COMMA",
-	TOKEN_COLON:         "COLON",
-	TOKEN_DOT:           "DOT",
-	TOKEN_LESS_ANGLE:    "LESS_ANGLE",
-	TOKEN_GREATER_ANGLE: "GREATER_ANGLE",
+	PLUS:          "+",
+	MINUS:         "-",
+	MULTIPLY:      "*",
+	DIVIDE:        "/",
+	MODULO:        "%",
+	ASSIGN:        "=",
+	EQUAL:         "==",
+	NOT_EQUAL:     "!=",
+	LESS:          "<",
+	LESS_EQUAL:    "<=",
+	GREATER:       ">",
+	GREATER_EQUAL: ">=",
+	LPAREN:        "(",
+	RPAREN:        ")",
+	LBRACE:        "{",
+	RBRACE:        "}",
+	LBRACKET:      "[",
+	RBRACKET:      "]",
+	SEMICOLON:     ";",
+	COMMA:         ",",
+	COLON:         ":",
+	DOT:           ".",
 }
 
-// Keywords map des mots-clés
-var Keywords = map[string]TokenType{
-	"action": TOKEN_ACTION,
-	"start":  TOKEN_START,
-	"stop":   TOKEN_STOP,
-	"end":    TOKEN_END,
+func (t TokenType) String() string {
+	if name, ok := tokenNames[t]; ok {
+		return name
+	}
+	return "UNKNOWN"
+}
 
-	"if":      TOKEN_IF,
-	"else":    TOKEN_ELSE,
-	"while":   TOKEN_WHILE,
-	"for":     TOKEN_FOR,
-	"foreach": TOKEN_FOREACH,
-	"switch":  TOKEN_SWITCH,
-	"case":    TOKEN_CASE,
-	"default": TOKEN_DEFAULT,
-
-	"let":      TOKEN_LET,
-	"const":    TOKEN_CONST,
-	"struct":   TOKEN_STRUCT,
-	"function": TOKEN_FUNCTION,
-	"return":   TOKEN_RETURN,
-
-	"select": TOKEN_SELECT,
-	"from":   TOKEN_FROM,
-	"where":  TOKEN_WHERE,
-	"order":  TOKEN_ORDER,
-	"by":     TOKEN_BY,
-	"asc":    TOKEN_ASC,
-	"desc":   TOKEN_DESC,
-	"limit":  TOKEN_LIMIT,
-	"offset": TOKEN_OFFSET,
-	"as":     TOKEN_AS,
-	"into":   TOKEN_INTO,
-	"objet":  TOKEN_OBJET,
-	"inner":  TOKEN_INNER,
-	"left":   TOKEN_LEFT,
-	"right":  TOKEN_RIGHT,
-	"join":   TOKEN_JOIN,
-	"on":     TOKEN_ON,
-	"group":  TOKEN_GROUP,
-	"having": TOKEN_HAVING,
-
-	"like":      TOKEN_LIKE,
-	"between":   TOKEN_BETWEEN,
-	"is":        TOKEN_IS,
-	"null":      TOKEN_NULL,
-	"union":     TOKEN_UNION,
-	"all":       TOKEN_ALL,
-	"with":      TOKEN_WITH,
-	"recursive": TOKEN_RECURSIVE,
-	"distinct":  TOKEN_DISTINCT,
-
-	"create":   TOKEN_CREATE,
-	"drop":     TOKEN_DROP,
-	"delete":   TOKEN_DELETE,
-	"update":   TOKEN_UPDATE,
-	"save":     TOKEN_SAVE,
-	"set":      TOKEN_SET,
-	"view":     TOKEN_VIEW,
-	"index":    TOKEN_INDEX,
-	"sequence": TOKEN_SEQUENCE,
-	"not":      TOKEN_NOT,
-	// "default":  TOKEN_DEFAULT,
-
-	"integer":  TOKEN_INTEGER,
-	"boolean":  TOKEN_BOOLEAN,
-	"float":    TOKEN_FLOAT_TYPE,
-	"string":   TOKEN_STRING_TYPE,
-	"date":     TOKEN_DATE,
-	"time":     TOKEN_TIME,
-	"datetime": TOKEN_DATETIME,
-	"duration": TOKEN_DURATION,
-	"array":    TOKEN_ARRAY,
-
-	"digits":         TOKEN_DIGITS,
-	"length":         TOKEN_LENGTH,
-	"min":            TOKEN_MIN,
-	"max":            TOKEN_MAX,
-	"range":          TOKEN_RANGE,
-	"integer_digits": TOKEN_INTEGER_DIGITS,
-	"decimal_digits": TOKEN_DECIMAL_DIGITS,
-
-	"optional": TOKEN_OPTIONAL,
-	"self":     TOKEN_SELF,
-
-	"years":        TOKEN_YEARS,
-	"months":       TOKEN_MONTHS,
-	"days":         TOKEN_DAYS,
-	"hours":        TOKEN_HOURS,
-	"minutes":      TOKEN_MINUTES,
-	"seconds":      TOKEN_SECONDS,
-	"milliseconds": TOKEN_MILLISECONDS,
-
-	"true":  TOKEN_TRUE,
-	"false": TOKEN_FALSE,
-
-	"And": TOKEN_AND,
-	"Or":  TOKEN_OR,
-	"Not": TOKEN_NOT_OP,
-
-	"in": TOKEN_IN,
+func (t Token) String() string {
+	return tokenNames[t.Type]
 }
